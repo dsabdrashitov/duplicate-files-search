f80530b8b2a65ac253c206153594242aab19cf79
dsabdrashitov <47863341+dsabdrashitov@users.noreply.github.com>
2024-03-24 01:21:26 +0300

    Remove descendants of scanned dir in db.

--- dfs/scan.py
+++ dfs/scan.py
@@ -1,5 +1,5 @@
 import logging
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 import os
 import hashlib
 from typing import List
@@ -44,6 +44,13 @@ def _process_file(db, base: Path, file_path: Path, progress: List[int]) -> None:
         _logger.info(f"progress {progress[0]} of ? (--%)")
 
 
+def _process_dir(db, base: Path, p: Path, progress: List[int], ignore: set):
+    _logger.info(f"{p} is dir. traversing")
+    for child in p.iterdir():
+        _scan_path(db, base, child, progress, ignore)
+    _logger.info(f"{p} done")
+
+
 def _scan_path(db, base: Path, p: Path, progress: List[int], ignore: set = None) -> None:
     assert p.is_absolute()
     if ignore and (str(p) in ignore):
@@ -52,10 +59,7 @@ def _scan_path(db, base: Path, p: Path, progress: List[int], ignore: set = None)
     if p.is_file():
         _process_file(db, base, p, progress)
     elif p.is_dir():
-        _logger.info(f"{p} is dir. traversing")
-        for child in p.iterdir():
-            _scan_path(db, base, child, progress, ignore)
-        _logger.info(f"{p} done")
+        _process_dir(db, base, p, progress, ignore)
 
 
 def _calc_size(p: Path, ignore=None) -> int:
@@ -76,6 +80,16 @@ def scan(db_file, path: str):
         base = Path(db.Info.get_value(ARG_BASE_PATH))
         ignore = {str(Path(db_file).absolute())}
         total_size = _calc_size(Path(path).absolute(), ignore=ignore)
+
+        # remove all prefixes
+        prefix = _normal(Path(path).absolute(), base)
+        prefix_dir = PurePosixPath(prefix)
+        potential_children = [hash_row for hash_row in db.Hashes.select().where(db.Hashes.path.startswith(prefix))]
+        for pc in potential_children:
+            if PurePosixPath(pc.path).is_relative_to(prefix_dir):
+                _logger.info(f"Remove item for: {pc.path}, debug:{prefix_dir}")
+                db.Hashes.delete().where(db.Hashes.path == pc.path)
+
         _scan_path(db, base, Path(path).absolute(), progress=[0, total_size], ignore=ignore)
     finally:
         db.close()
