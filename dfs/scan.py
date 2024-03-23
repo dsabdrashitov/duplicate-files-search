2d6f131ffb9f3d601b0740b122af4d42b7ad5558
dsabdrashitov <47863341+dsabdrashitov@users.noreply.github.com>
2024-03-24 00:57:15 +0300

    Add progress calculation in terms of total bytes processed.

--- dfs/scan.py
+++ dfs/scan.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 import os
 import hashlib
+from typing import List
 from .database import DatabaseConnection, ARG_BASE_PATH
 
 _logger = logging.getLogger(__name__)
@@ -13,8 +14,9 @@ def _normal(path: Path, base: Path) -> str:
     return path.relative_to(base).as_posix()
 
 
-def _process_file(db, base: Path, file_path: Path) -> None:
+def _process_file(db, base: Path, file_path: Path, progress: List[int]) -> None:
     size = os.path.getsize(file_path)
+    _logger.info(f"{file_path} is file. size = {size}. calculating checksum")
     h = hashlib.md5()
     print('[', end='', flush=True)
     with file_path.open("rb") as f:
@@ -34,30 +36,47 @@ def _process_file(db, base: Path, file_path: Path) -> None:
     hash_hex = h.hexdigest()
     record_path = _normal(file_path, base)
     db.Hashes.record(record_path, False, 1, size, hash_hex)
-    _logger.info(f"{record_path}({size}): {hash_hex}")
+    _logger.info(f"{record_path}: {hash_hex}")
+    progress[0] = progress[0] + size
+    if progress[1] > 0:
+        _logger.info(f"progress {progress[0]} of {progress[1]} ({progress[0] * 100 / progress[1]:.2f}%)")
+    else:
+        _logger.info(f"progress {progress[0]} of ? (--%)")
 
 
-def _scan_path(db, base: Path, p: Path, ignore=None):
+def _scan_path(db, base: Path, p: Path, progress: List[int], ignore: set = None) -> None:
     assert p.is_absolute()
-    if str(p) in ignore:
+    if ignore and (str(p) in ignore):
         _logger.info(f"{p} ignored.")
         return
     if p.is_file():
-        _logger.info(f"{p} is file. calculating checksum")
-        _process_file(db, base, p)
+        _process_file(db, base, p, progress)
     elif p.is_dir():
         _logger.info(f"{p} is dir. traversing")
         for child in p.iterdir():
-            _scan_path(db, base, child, ignore)
+            _scan_path(db, base, child, progress, ignore)
         _logger.info(f"{p} done")
+
+
+def _calc_size(p: Path, ignore=None) -> int:
+    result = 0
+    if ignore and (str(p) in ignore):
+        pass
+    elif p.is_file():
+        result = os.path.getsize(p)
+    elif p.is_dir():
+        for child in p.iterdir():
+            result = result + _calc_size(child, ignore)
+    return result
 
 
 def scan(db_file, path: str):
     db = DatabaseConnection(db_file)
-    base = Path(db.Info.get_value(ARG_BASE_PATH))
-    ignore = {str(Path(db_file).absolute())}
     try:
-        _scan_path(db, base, Path(path).absolute(), ignore=ignore)
+        base = Path(db.Info.get_value(ARG_BASE_PATH))
+        ignore = {str(Path(db_file).absolute())}
+        total_size = _calc_size(Path(path).absolute(), ignore=ignore)
+        _scan_path(db, base, Path(path).absolute(), progress=[0, total_size], ignore=ignore)
     finally:
         db.close()
     _logger.info(f"scan {path} to {db_file} done")
