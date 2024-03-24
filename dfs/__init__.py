01c0f0288cd2c1b73bba42705ad697f953f6cb9b
dsabdrashitov <47863341+dsabdrashitov@users.noreply.github.com>
2024-03-25 02:00:39 +0300

    Add commands for working with files collections. For example for creating ignore set.

--- dfs/__init__.py
+++ dfs/__init__.py
@@ -1,1 +1,2 @@
 from .hashes_file import FileHash, load_hashes, save_hashes
+from .files_file import load_files_collection, save_files_collection
--- dfs/__main__.py
+++ dfs/__main__.py
@@ -9,28 +9,29 @@ def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="command")
 
-    parser_init = subparsers.add_parser("init")
-    parser_init.add_argument("db", help="path to file with hashes")
+    parser_init = subparsers.add_parser("init", help="init file with hashes collection")
+    parser_init.add_argument("hashes", help="path to file with hashes")
 
-    parser_scan = subparsers.add_parser("scan")
-    parser_scan.add_argument("db", help="path to file with database")
+    parser_scan = subparsers.add_parser("scan", help="scan path and add it's subtree to hashes collection")
+    parser_scan.add_argument("hashes", help="path to file with hashes (will be overwritten)")
     parser_scan.add_argument("path", help="path to the files to be scanned")
+    parser_scan.add_argument("-i", "--ignore", help="")
     parser_scan.add_argument("-f", "--fast", action='store_true', help="option for skip hash calculation for known "
                                                                        "files")
 
-    parser_scan = subparsers.add_parser("ignore")
-    ignore_sub = parser_scan.add_subparsers(dest="subcommand")
-    # list
-    parser_scan_list = ignore_sub.add_parser("list")
-    parser_scan_list.add_argument("db", help="path to file with database")
-    # add
-    parser_scan_add = ignore_sub.add_parser("add")
-    parser_scan_add.add_argument("db", help="path to file with database")
-    parser_scan_add.add_argument("path", help="path to the files to be ignored")
-    # remove
-    parser_scan_remove = ignore_sub.add_parser("remove")
-    parser_scan_remove.add_argument("db", help="path to file with database")
-    parser_scan_remove.add_argument("path", help="path to be removed from ignore list")
+    parser_files = subparsers.add_parser("files", help="manipulations with lists of files")
+    files_sub = parser_files.add_subparsers(dest="subcommand")
+    # init
+    parser_files_init = files_sub.add_parser("init", help="create empty")
+    parser_files_init.add_argument("list", help="path to file with list")
+    # add file
+    parser_files_add = files_sub.add_parser("add")
+    parser_files_add.add_argument("list", help="path to file with list")
+    parser_files_add.add_argument("path", help="path to be added")
+    # remove file
+    parser_files_remove = files_sub.add_parser("remove")
+    parser_files_remove.add_argument("list", help="path to file with list")
+    parser_files_remove.add_argument("path", help="path to be removed")
 
     args = parser.parse_args()
     if args.command == "init":
@@ -42,20 +43,23 @@ def main():
         # path = args.path
         # fast = args.fast
         # dfs.scan(db_file, path, fast=fast)
-    elif args.command == "ignore":
-        pass
-        # db_file = args.db
-        # if args.subcommand == "list":
-        #     # dfs.ignore_list(db_file)
-        # elif args.subcommand == "add":
-        #     path = args.path
-        #     # dfs.ignore_add(db_file, path)
-        # elif args.subcommand == "remove":
-        #     path = args.path
-        #     # dfs.ignore_remove(db_file, path)
-        # else:
-        #     _logger.error(f"Unknown subcommand: ignore {args.subcommand}")
-        #     exit(1)
+    elif args.command == "files":
+        files_file = args.list
+        if args.subcommand == "init":
+            dfs.save_files_collection(files_file, [])
+        elif args.subcommand == "add":
+            path = args.path
+            files_set = dfs.load_files_collection(files_file)
+            files_set.add(path)
+            dfs.save_files_collection(files_file, files_set)
+        elif args.subcommand == "remove":
+            path = args.path
+            files_set = dfs.load_files_collection(files_file)
+            files_set.remove(path)
+            dfs.save_files_collection(files_file, files_set)
+        else:
+            _logger.error(f"Unknown subcommand: files {args.subcommand}")
+            exit(1)
     else:
         _logger.error(f"Unknown command: {args.command}")
         exit(1)
